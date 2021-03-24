4efbce3f727baac2d378f10323fbf099d830b45c
Verenasworld <verena.felizidas@gmail.com>
2021-03-24 16:28:19 +0100

    add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './share/material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { FormComponent } from './component/form/form.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
